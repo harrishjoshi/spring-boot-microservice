2027012d318c572f72514108c158e47d58d76c42
Harish <dev.harishjoshi@gmail.com>
2023-03-19 08:51:39 +0545

    spring boot security and oauth2 impl

---
+++ discovery-server/src/main/java/com/harrishjoshi/discoveryserver/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.harrishjoshi.discoveryserver.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Value("${app.eureka.username}")
+    private String username;
+    @Value("${app.eureka.password}")
+    private String password;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User
+                .withUsername(username)
+                .password(password)
+                .authorities("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests().anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+
+        return http.build();
+    }
+}
